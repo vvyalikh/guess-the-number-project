59d2e67fb7876d607bd53ab311e56c0f000e3dfc
vvyalikh <>
2021-05-04 17:25:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
+import random
+game_over = False
+th_number = random.choice(range(1, 100))
+
+
+print('Welcome to the number guessing game!')
+print("I'm thinking number from 1 to 100")
+print(th_number)
+difficulty = input("Please choose difficulty. Type 'hard' or 'easy':\n")
+
+count_hard = 5
+count_easy = 10
+if difficulty == "hard":
+  print(f"You have {count_hard} attempts")
+  guess = int(input("Make a guess:\n"))
+  while count_hard != 0:
+    if guess == th_number:
+      count_hard = 0
+      print("Yes, that's right number! You win!")    
+    if guess < th_number:
+      print("Too low.")
+      count_hard -= 1
+      print(f"You have {count_hard} attempts left")
+      guess = int(input("Make a guess:\n"))
+    elif guess > th_number:
+      print("Too hight")
+      count_hard -= 1
+      print(f"You have {count_hard} attempts left")
+      guess = int(input("Make a guess:\n"))
+if difficulty == "easy":
+  print(f"You have {count_easy} attempts")
+  guess = int(input("Make a guess:\n"))
+  while count_easy != 0:
+    if guess == th_number:
+      count_easy = 0
+      print("Yes, that's right number! You win!")    
+    if guess < th_number:
+      print("Too low.")
+      count_easy -= 1
+      print(f"You have {count_easy} attempts left")
+      guess = int(input("Make a guess:\n"))
+    elif guess > th_number:
+      print("Too hight")
+      count_easy -= 1
+      print(f"You have {count_easy} attempts left")
+      guess = int(input("Make a guess:\n"))    
+    
+      
+
+
+
+# else:
+#   print("Something goes wrong. Please try again.")
+
+
+
+
